063d276682e50e6860ada9c0e2bc27fe7aa6fe26
Yannick Heinrich <yannick.heinrich@gmail.com>
2017-03-01 11:29:46 +0100

    Initial Commit

---
+++ APIKit/APIKit.h
@@ -0,0 +1,19 @@
+//
+//  APIKit.h
+//  APIKit
+//
+//  Created by Yannick Heinrich on 01.03.17.
+//  Copyright © 2017 yageek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIKit.
+FOUNDATION_EXPORT double APIKitVersionNumber;
+
+//! Project version string for APIKit.
+FOUNDATION_EXPORT const unsigned char APIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIKit/PublicHeader.h>
+
+
